51474d4cb8500df8b90ede943be4b06f38638335
octaspire <octaspire@octaspire.com>
2020-10-11 23:36:36 +0300

    Add example and initial test runner

---
+++ doc/example/publish.el
@@ -0,0 +1,17 @@
+(load "~/ox-base64-html/ox-base64-html.el")
+(require 'ob-css)
+(require 'ox-publish)
+
+(setq org-publish-project-alist
+      '(("org"
+	 :base-directory "~/ox-base64-html/doc/example/"
+	 :base-extension "org"
+	 :htmlized-source t
+	 :recursive t
+	 :publishing-directory "~/www/"
+	 :publishing-function octaspire/ox-base64-html-publish)
+	("octaspire" :components ("org"))))
+
+(defun octaspire/publish ()
+  (interactive)
+  (org-publish "octaspire" t))
